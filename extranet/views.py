56f9c8b102847b0719de27df13957a854101798f
jvkoob <60754399+jvkoob@users.noreply.github.com>
2020-12-07 20:08:18 +0100

    wstepne logowanie (#5)
    Co-authored-by: jakubmisiak@icloud.com <kuba@DESKTOP-DGH36BF.localdomain>

--- extranet/views.py
+++ extranet/views.py
@@ -1,8 +1,25 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login
 
 
-# Create your views here.
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
+
+
 
 def index(request):
     XD = '123'
