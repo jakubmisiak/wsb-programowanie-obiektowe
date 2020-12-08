3792e3294c819944b35451151f6e67ca94e6271f
jvkoob <60754399+jvkoob@users.noreply.github.com>
2020-12-08 18:11:22 +0100

    Views update (#7)
    * wstepne logowanie
    
    * Dodatkowe viewsy z poprawnym logowaniem i prostym mapowaniem danych użytkownika
    
    * Update extranet/models/student.py
    
    Co-authored-by: Łukasz Kliś <lukasz@klis.pl>
    
    * Update extranet/models/teacher.py
    
    Co-authored-by: Łukasz Kliś <lukasz@klis.pl>
    
    * Update extranet/urls.py
    
    Co-authored-by: Łukasz Kliś <lukasz@klis.pl>
    
    * Update extranet/urls.py
    
    Co-authored-by: Łukasz Kliś <lukasz@klis.pl>
    
    * Update extranet/views.py
    
    Co-authored-by: Łukasz Kliś <lukasz@klis.pl>
    
    * Update extranet/views.py
    
    Co-authored-by: Łukasz Kliś <lukasz@klis.pl>
    
    Co-authored-by: jakubmisiak@icloud.com <kuba@DESKTOP-DGH36BF.localdomain>
    Co-authored-by: Łukasz Kliś <lukasz@klis.pl>

--- extranet/urls.py
+++ extranet/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
 
     path('index', views.index, name='index'),
-    path('login', views.user_login, name='login' )
+    path('login', views.user_login, name='login'),
+    path('homes', views.student_data, name='homes'),
+    path('homet', views.teacher_data, name='homet'),
 
 ]
--- extranet/views.py
+++ extranet/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login
-
-
+from extranet.models import teacher
+from extranet.models.student import Student
+from extranet.models.teacher import Teacher
 
 
 def user_login(request):
@@ -12,7 +14,13 @@ def user_login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('index')
+
+            print(user)
+            if Teacher.objects.filter(user=user).exists():
+                return redirect('homet')
+            else:
+                return redirect('homes')
+
         else:
             return redirect('login')
 
@@ -21,6 +29,29 @@ def user_login(request):
 
 
 
+
 def index(request):
     XD = '123'
     return render(request, 'index.html', {'XD':XD})
+
+@login_required
+def teacher_data(request):
+    current_user = request.user
+    if Teacher.objects.filter(user=current_user).exists():
+        log_teacher = Teacher.objects.get()
+        current_title = log_teacher.title
+        print(current_title)
+
+    text = 'Jestem nauczycielem'
+    return render(request, 'teacherData.html', {'curent_title':current_title})
+
+@login_required
+def student_data(request):
+    current_user = request.user
+    if Student.objects.filter(user=current_user).exists():
+        log_student = Student.objects.get()
+        current_index = log_student.index
+        current_group = log_student.group
+        print(current_index)
+    text = 'Jestem studentem'
+    return render(request,'userData.html', {'current_index':current_index})
